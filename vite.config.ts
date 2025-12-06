76ae702d251315b96469186aaf14a8940b4c3006
Fakih Hamid <excaprodz@protonmail.com>
2025-12-07 07:40:00 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   server: {
     host: "::",
     port: 8080,
